6deef49f2965ecce9567f308b6c4db94a1d7fa46
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-14 00:42:07 +0100

    order nodes alphabetically

--- src/org/mql/jcodeeditor/OrderedTreeModel.java
+++ src/org/mql/jcodeeditor/OrderedTreeModel.java
@@ -12,58 +12,62 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Vector;
 
-
 public class OrderedTreeModel extends DefaultTreeModel {
 
-    public OrderedTreeModel(TreeNode root) {
-        super(root);
-    }
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    public void insertNodeInto(MutableTreeNode newChild, MutableTreeNode parent, int index) {
-        super.insertNodeInto(newChild, parent, index);
-        sortChildren(parent);
-    }
+	public OrderedTreeModel(TreeNode root) {
+		super(root);
+	}
 
-    @Override
-    public void nodeStructureChanged(TreeNode node) {
-        super.nodeStructureChanged(node);
-        sortChildren((MutableTreeNode) node);
-    }
+	@Override
+	public void insertNodeInto(MutableTreeNode newChild, MutableTreeNode parent, int index) {
+		super.insertNodeInto(newChild, parent, index);
+		sortChildren(parent);
+	}
 
-    private void sortChildren(MutableTreeNode parent) {
-        int childCount = parent.getChildCount();
-        if (childCount <= 1) {
-            return;
-        }
+	@Override
+	public void nodeStructureChanged(TreeNode node) {
+		super.nodeStructureChanged(node);
+		sortChildren((MutableTreeNode) node);
+	}
 
-        List<MutableTreeNode> children = new Vector<>();
-        for (int i = 0; i < childCount; i++) {
-            children.add((MutableTreeNode) parent.getChildAt(i));
-        }
+	private void sortChildren(MutableTreeNode parent) {
+		int childCount = parent.getChildCount();
+		if (childCount <= 1) {
+			return;
+		}
 
-        Collections.sort(children, new Comparator<MutableTreeNode>() {
-            @Override
-            public int compare(MutableTreeNode o1, MutableTreeNode o2) {
-            	//- first is less
-            	File f1 =  (File)((DefaultMutableTreeNode) o1).getUserObject();
-            	File f2 =  (File)((DefaultMutableTreeNode) o2).getUserObject();
-            	if(f1.isDirectory() && f2.isFile()) {
-            		return 1;
-            	}else if(f2.isDirectory() && f1.isFile()) {
-            		return -1;
-            	}else {
-            		return f1.getName().compareTo(f2.getName());
-            	}
+		List<MutableTreeNode> children = new Vector<>();
+		for (int i = 0; i < childCount; i++) {
+			children.add((MutableTreeNode) parent.getChildAt(i));
+		}
+
+		Collections.sort(children, new Comparator<MutableTreeNode>() {
+			@Override
+			public int compare(MutableTreeNode o1, MutableTreeNode o2) {
+				// - first is less
+				File f1 = (File) ((DefaultMutableTreeNode) o1).getUserObject();
+				File f2 = (File) ((DefaultMutableTreeNode) o2).getUserObject();
+				if (f1.isDirectory() && f2.isFile()) {
+					return -1;
+				} else if (f2.isDirectory() && f1.isFile()) {
+					return 1;
+				} else {
+					return f1.getName().compareTo(f2.getName());
+				}
 //            	if()
 //                return o1.toString().compareTo(o2.toString()); // Customize this comparator as needed
-            }
-        });
-
-        ((DefaultMutableTreeNode) parent).removeAllChildren();
-        for (MutableTreeNode child : children) {
-            parent.insert(child, parent.getChildCount());
-        }
-        super.nodeStructureChanged(parent);
-    }
+			}
+		});
+		System.out.println(children);
+		((DefaultMutableTreeNode) parent).removeAllChildren();
+		for (MutableTreeNode child : children) {
+			parent.insert(child, parent.getChildCount());
+		}
+		super.nodeStructureChanged(parent);
+	}
 }
