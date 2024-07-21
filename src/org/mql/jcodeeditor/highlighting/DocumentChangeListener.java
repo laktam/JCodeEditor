11b13fba38dbf46f472ccc490118e373c8e52a43
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-21 16:50:30 +0100

    add document change listener

---
+++ src/org/mql/jcodeeditor/highlighting/DocumentChangeListener.java
@@ -0,0 +1,38 @@
+package org.mql.jcodeeditor.highlighting;
+
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentEvent.EventType;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+
+import org.mql.jcodeeditor.utils.Styles;
+
+public class DocumentChangeListener implements DocumentListener {
+
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		// defer the style application until after the current event dispatch is
+		// complete.
+		SwingUtilities.invokeLater(() -> {
+			Styles.applyStyles();
+		});
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		// defer the style application until after the current event dispatch is
+		// complete.
+		SwingUtilities.invokeLater(() -> {
+			Styles.applyStyles();
+		});
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
