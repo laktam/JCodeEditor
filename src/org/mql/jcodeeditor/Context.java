e82c7093b041288ff9c6cff7827fe81e211af44a
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-24 14:57:13 +0100

    before moving to maven and using OSGI

--- src/org/mql/jcodeeditor/Context.java
+++ src/org/mql/jcodeeditor/Context.java
@@ -1,6 +1,12 @@
 package org.mql.jcodeeditor;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLClassLoader;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -8,10 +14,20 @@ import org.mql.jcodeeditor.highlighting.Tokenizer;
 
 public class Context {
 	// string : file extensions
-	public static Map<String, Tokenizer> tokenizersMap = new HashMap<String, Tokenizer>();
+	private static Map<String, Tokenizer> tokenizersMap = new HashMap<String, Tokenizer>();
 	
-	// this should be called to search for plugins related stuff
-	public void init() {
+	static {
 		
+		init();
 	}
+	// this should be called to search for plugins related stuff
+	public static void init() {
+		//load classes
+//		findAllImplementations()
+	}
+	
+	
+	
+	
+	
 }
