9b36f258121788a387f8eb7fc5ed369b618f8dce
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-26 03:00:36 +0100

    make pluging loading generic to load implementation for specific intefaces

--- src/main/java/org/mql/jcodeeditor/plugins/PluginLoader.java
+++ src/main/java/org/mql/jcodeeditor/plugins/PluginLoader.java
@@ -11,37 +11,40 @@ import org.mql.jcodeeditor.highlighting.Highlighter;
 import org.mql.jcodeeditor.highlighting.Tokenizer;
 
 public class PluginLoader {
-    private static final String PLUGINS_DIR = "./plugins";
+	private static final String PLUGINS_DIR = "plugins/";
 
-    public static <T> List<T> loadPlugins(Class<T> pluginType) {
+	public static <T> List<T> loadPlugins(Class<T> pluginType) {
 //    	List<Tokenizer> tokenizers = new Vector<Tokenizer>();
 //    	List<Highlighter> highlighter= new Vector<Highlighter>();
-    	List<T> implementations = new Vector<>();
-        File pluginsDir = new File(PLUGINS_DIR);
-        if (!pluginsDir.exists() || !pluginsDir.isDirectory()) {
-            System.out.println("Plugins directory not found or is not a directory.");
-            return null;
-        }
+		List<T> implementations = new Vector<>();
+		File pluginsDir = new File(PLUGINS_DIR);
+		if (!pluginsDir.exists() || !pluginsDir.isDirectory()) {
+			System.out.println("Plugins directory not found or is not a directory.");
+			return null;
+		}
 
-        File[] jarFiles = pluginsDir.listFiles((dir, name) -> name.endsWith(".jar"));
+		File[] jarFiles = pluginsDir.listFiles((dir, name) -> name.endsWith(".jar"));
 
-        if (jarFiles != null) {
-            for (File jar : jarFiles) {
-                try {
-                    URL jarUrl = jar.toURI().toURL();
-                    URLClassLoader classLoader = new URLClassLoader(new URL[]{jarUrl}, PluginLoader.class.getClassLoader());
-                    ServiceLoader<T> loader = ServiceLoader.load(pluginType, classLoader);                    
-                    for (T plugin : loader) {
-                        System.out.println("Loaded plugin: " + plugin.getClass().getName());
-                        implementations.add(plugin);
-                    }
-                } catch (Exception e) {
-                    System.err.println("Error loading plugin from " + jar.getName() + ": " + e.getMessage());
-                }
-            }
-        } else {
-            System.out.println("No plugin JARs found in the plugins directory.");
-        }
-        return implementations;
-    }
+		if (jarFiles != null) {
+			for (File jar : jarFiles) {
+				System.out.println("Loading JAR: " + jar.getAbsolutePath());
+
+				try {
+					URL jarUrl = jar.toURI().toURL();
+					URLClassLoader classLoader = new URLClassLoader(new URL[] { jarUrl },
+							PluginLoader.class.getClassLoader());
+					ServiceLoader<T> loader = ServiceLoader.load(pluginType, classLoader);
+					for (T plugin : loader) {
+						System.out.println("Loaded plugin: " + plugin.getClass().getName());
+						implementations.add(plugin);
+					}
+				} catch (Exception e) {
+					System.err.println("Error loading plugin from " + jar.getName() + ": " + e.getMessage());
+				}
+			}
+		} else {
+			System.out.println("No plugin JARs found in the plugins directory.");
+		}
+		return implementations;
+	}
 }
