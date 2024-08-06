52b005136922a6c945753427a1f6589939acfe7f
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-06 02:01:00 +0100

    add FilesHandler interface for plugins that need to use explorer files

---
+++ src/main/java/org/mql/jcodeeditor/plugins/FilesHandler.java
@@ -0,0 +1,9 @@
+package org.mql.jcodeeditor.plugins;
+
+import java.io.File;
+import java.util.List;
+
+public interface FilesHandler {
+	// maybe only use addFile
+	public void addFile(File file);
+}
