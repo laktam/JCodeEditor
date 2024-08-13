3eca11d391a2e1feb5f864ba7d26c225eaa00b93
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-13 15:27:28 +0100

    add PropertiesHandler, an interface implemented by plugins that need to read and write properties

---
+++ src/main/java/org/mql/jcodeeditor/plugins/PropertiesHandler.java
@@ -0,0 +1,7 @@
+package org.mql.jcodeeditor.plugins;
+
+import org.mql.jcodeeditor.properties.PropertiesManager;
+
+public interface PropertiesHandler {
+		public void setPropertiesManager(PropertiesManager propertiesManager);
+}
